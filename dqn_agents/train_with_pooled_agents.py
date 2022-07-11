a55e4032922b9e1b686f30d3c6d40b6dc22d4dd2
am-bean <88600346+am-bean@users.noreply.github.com>
2022-07-11 10:52:54 +0000

    Fixed logging to allow run to finish

--- dqn_agents/train_with_pooled_agents.py
+++ dqn_agents/train_with_pooled_agents.py
@@ -1,6 +1,7 @@
 """Loads a previously trained set of agents and then continues to train one of them.
 """
 
+import re
 import argparse
 import os
 import platform
@@ -26,22 +27,25 @@ torch, nn = try_import_torch()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--stop-iters", type=int, default=40000)
-parser.add_argument('--checkpoint_freq', type=int, default=1000)
+parser.add_argument("--stop-iters", type=int, default=4)
+parser.add_argument('--checkpoint_freq', type=int, default=1)
 parser.add_argument("--num-cpus", type=int, default=4)
 parser.add_argument("--repetitions", type=int, default=6)
+parser.add_argument("--checkpoint", type=str, default='l2_4')
 parser.add_argument('--cp-filepath', type=str, default='C:/Users/Administrator/ray_results/DQN/')
 parser.add_argument('--local-folder', type=str, default="/tsclient/C/Users/Andre/ray_results/DQN/aws")
-parser.add_argument('--checkpoint-name', type=str, default='/checkpoint_040000/checkpoint-40000')
-parser.add_argument('--shutdown', type=bool, default=True)
+parser.add_argument('--shutdown', type=bool, default=False)
+parser.add_argument('--copy_to_local', type=bool, default=False)
 parser.add_argument('--use-checkpoints', type=bool, default=True)
 
 if __name__ == "__main__":
 
-    checkpoints_pool = ['l1_4', 'l2_4', 'l3_4', 'l4_4']
-    raw_checkpoints_pool = checkpoints_pool.copy()
-
     args = parser.parse_args()
+
+    checkpoints_pool = ['l1_4', 'l2_4', 'l3_4', 'l4_4']
+    checkpoints_pool.append(args.checkpoint)
+    checkpoints_pool = list(set(checkpoints_pool))
+    raw_checkpoints_pool = checkpoints_pool.copy()
 
     subs = {'a': '\checkpoint_010000\checkpoint-10000',
             'b': '\checkpoint_020000\checkpoint-20000',
@@ -52,51 +56,49 @@ if __name__ == "__main__":
             for sub in subs.keys():
                 checkpoints_pool.append(checkpoint + sub)
 
-    # Get obs- and action Spaces.
-    def env_creator():
-        env = cards_env.env()
-        return env
-
-
-    ModelCatalog.register_custom_model("masked_dqn", TorchMaskedActions)
-    register_env("cards", lambda config: PettingZooEnv(env_creator()))
-
-    my_env = PettingZooEnv(env_creator())
-    obs_space = my_env.observation_space
-    act_space = my_env.action_space
-
-    # Setup DQN with an ensemble of `num_policies` different policies.
-
-    old_config = default_config()
-
-    old_config["env"] = "cards"
-    old_config["multiagent"] = {
-        "policies": {
-            "player_0": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
-            "player_1": (MaskedRandomPolicy, obs_space, act_space, {}),
-            "player_2": (MaskedRandomPolicy, obs_space, act_space, {}),
-            "player_3": (MaskedRandomPolicy, obs_space, act_space, {}),
-        },
-        "policy_mapping_fn": lambda agent_id: agent_id,
-        "policies_to_train": ["player_0"],
-    }
-
-    new_config = deepcopy(old_config)
-    new_config["multiagent"] = {
-        "policies": {
-            "player_0": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
-            "player_1": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
-            "player_2": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
-            "player_3": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
-        },
-        "policy_mapping_fn": lambda agent_id: agent_id,
-        "policies_to_train": ["player_0", "player_1", "player_2", "player_3"],
-    }
-
-
     for i in range(args.repetitions):
 
         ray.init(num_cpus=args.num_cpus)
+
+        # Get obs- and action Spaces.
+        def env_creator():
+            env = cards_env.env()
+            return env
+
+        ModelCatalog.register_custom_model("masked_dqn", TorchMaskedActions)
+        register_env("cards", lambda config: PettingZooEnv(env_creator()))
+
+        my_env = PettingZooEnv(env_creator())
+        obs_space = my_env.observation_space
+        act_space = my_env.action_space
+
+        # Setup DQN with an ensemble of `num_policies` different policies.
+
+        old_config = default_config()
+
+        old_config["env"] = "cards"
+        old_config["multiagent"] = {
+            "policies": {
+                "player_0": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
+                "player_1": (MaskedRandomPolicy, obs_space, act_space, {}),
+                "player_2": (MaskedRandomPolicy, obs_space, act_space, {}),
+                "player_3": (MaskedRandomPolicy, obs_space, act_space, {}),
+            },
+            "policy_mapping_fn": lambda agent_id: agent_id,
+            "policies_to_train": ["player_0"],
+        }
+
+        new_config = deepcopy(old_config)
+        new_config["multiagent"] = {
+            "policies": {
+                "player_0": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
+                "player_1": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
+                "player_2": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
+                "player_3": (None, obs_space, act_space, {"model": {"custom_model": "masked_dqn"}}),
+            },
+            "policy_mapping_fn": lambda agent_id: agent_id,
+            "policies_to_train": ["player_0"],
+        }
 
         # Create a dummy Trainer to load our checkpoint.
         dummy_trainer = DQNTrainer(config=old_config)
@@ -120,7 +122,10 @@ if __name__ == "__main__":
         weights_dict = {}
         shuffle(checkpoints_pool)
         for j, pid in enumerate(untrained_weights.keys()):
-            weights_dict[pid] = weights_list[checkpoints_pool[j]]
+            if pid == 'player_0':
+                weights_dict[pid] = weights_list[args.checkpoint]
+            else:
+                weights_dict[pid] = weights_list[checkpoints_pool[j]]
         new_trainer.set_weights(weights_dict)
         # Create the checkpoint from which tune can pick up the
         # experiment.
@@ -142,12 +147,10 @@ if __name__ == "__main__":
 
         filepath = args.cp_filepath.replace('/', '\\')
         file = cp.local_path.replace(filepath, "")
-        checkpoint_name = args.checkpoint_name
-        folder = cp.local_path.replace(checkpoint_name.replace('/', '\\'), "")
+        folder = re.sub(r"checkpoint.*$", "", cp.local_path)
         folder_only = folder.replace(filepath, "")
 
-        machine = platform.uname()[1]
-        if machine != 'AndrewXPS15':
+        if args.copy_to_local:
             src_folder = folder
             dst_folder = args.local_folder + '/' + folder_only
             dst_folder = dst_folder.replace('/', '\\')
@@ -157,12 +160,6 @@ if __name__ == "__main__":
                 print(f'Copied to {dst_folder}')
             except:
                 pass
-            unneeded_files = ['params.json', 'params.pkl', 'progress.csv', 'result.json']
-            for f in unneeded_files:
-                try:
-                    os.remove(folder + '/' + f)
-                finally:
-                    pass
 
         trained_weights = new_trainer.get_weights()
         for j, pid in enumerate(trained_weights.keys()):
