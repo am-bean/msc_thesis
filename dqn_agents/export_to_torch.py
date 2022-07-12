74d35b447d95fd30e632aadb05ba8a0107875e08
am-bean <88600346+am-bean@users.noreply.github.com>
2022-07-12 10:27:57 +0100

    Exported models to onnx format

--- dqn_agents/export_to_torch.py
+++ dqn_agents/export_to_torch.py
@@ -1,10 +1,9 @@
 from card_game_files.load_pretrained_player import load_pretrained_player
+from best_checkpoints import best_checkpoints
 
 if __name__ == '__main__':
-    e, t = load_pretrained_player("C:/Users/Andre/ray_results/DQN/" + 'DQN_cards_4c2f7_00000_0_2022-06-06_17-54-15/checkpoint_009000/checkpoint-9000')
-    export_dir = 'C:/Users/Andre/OneDrive/Documents/GitHub/msc_thesis/dqn_agents/'
-    print(t.get_weights())
+    e, t = load_pretrained_player('C:/Users/Andre/ray_results/DQN/aws/' + best_checkpoints()['fcp'])
+    export_dir = 'C:/Users/Andre/OneDrive/Documents/GitHub/msc_thesis/dqn_agents/final_models'
     p = t.get_policy(policy_id='player_0')
     print(p.observation_space)
-    print(p.model)
-    print(p.get_weights())
+    p.export_model(export_dir=export_dir, onnx=12)
