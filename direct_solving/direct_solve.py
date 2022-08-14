3dac97cf4c9a84caf4feca2caacb65ce943cd2cd
am-bean <88600346+am-bean@users.noreply.github.com>
2022-08-14 17:20:14 +0100

    update solver

--- direct_solving/direct_solve.py
+++ direct_solving/direct_solve.py
@@ -3,95 +3,23 @@ import time
 import queue # imported for using queue.Empty exception
 import numpy as np
 from numba import jit
+import argparse
 
-from new_state import newState
-from get_score import getScore
-from get_actions import getActions
+from create_nearly_solved_initial_state import customInitialState
+from state_value import do_job
 from create_random_state import createRandomInitialState
 from hash_state import hashState
 
+parser = argparse.ArgumentParser()
 
-def do_job(states_to_run):
-    try_again = 0
-    last_printed = -999
-    # Continue to pull new states from the queue until they are exhausted
-    while True:
-        # This will raise queue.Empty if the queue is empty
-        try:
-            state = states_to_run.get_nowait()
-        # At the beginning there will only be one item in the queue, so I have a try again so that the 2-n processes
-        # don't quit immediately
-        except queue.Empty:
-            if try_again < 1000:
-                print('Process ended')
-                break
-            time.sleep(.1)
-            try_again += 1
-        # Otherwise do the task
-        else:
-            # Seems to be a delay in getting things into the queue
-            try_again = 0
-            obs, hands, seat, targetSeat, stateDict, randomSeats, childrenDict, parentDict = state
-            s = len(parentDict.keys())
-            if s - last_printed > 1000:
-                print(s)
-                last_printed = s
-            h = hashState(obs, hands)
-
-            # If the state has been solved before, don't do it again
-            if h in stateDict.keys():
-                continue
-
-            hand = hands[seat, :]
-            round = 6 - len(np.nonzero(hand)[0])
-
-            # If the game is over, compute the scores from the observation
-            if round == 6:
-                score = getScore(obs, hands, targetSeat)
-                # Save the learned value
-                stateDict[h] = score
-                # See if parent can be computed now, if so add back to queue
-                parent = parentDict[h]
-                p_obs, p_hands, p_seat, p_targetSeat, p_stateDict, p_randomSeats, p_childrenDict, p_parentDict = parent
-                parentHash = hashState(p_obs, p_hands)
-                if all(child in stateDict.keys() for child in childrenDict[parentHash]):
-                    states_to_run.put(parent)
-                continue
-
-            # Find all the children and update the parent and children dicts
-            actions = getActions(obs, hands, seat)
-            nextStates = [newState(obs, hands, action, seat) for action in actions]
-            children = [hashState(new_obs, new_hands) for (new_obs, new_hands, _) in nextStates]
-            childrenDict[h] = children
-            for child in children:
-                parentDict[child] = state
-
-            # If the child states are already computed, use them
-            if all(child in stateDict.keys() for child in children):
-                # Values are computed with respect to the non-random seats
-                # If it is a random turn we average since the random is uniform
-                if seat in randomSeats:
-                    score = np.mean([stateDict[child] for child in children])
-                else:
-                    score = np.max([stateDict[child] for child in children])
-                stateDict[h] = score
-                # See if parent can be computed now, if so add back to queue
-                parent = parentDict[h]
-                p_obs, p_hands, p_seat, p_targetSeat, p_stateDict, p_randomSeats, p_childrenDict, p_parentDict = parent
-                parentHash = hashState(p_obs, p_hands)
-                if all(child in stateDict.keys() for child in childrenDict[parentHash]):
-                    states_to_run.put(parent)
-                continue
-            # Otherwise send the child states to the compute queue
-            else:
-                for new_obs, new_hands, new_seat in nextStates:
-                    add_state = (new_obs, new_hands, new_seat, targetSeat, stateDict, randomSeats, childrenDict, parentDict)
-                    states_to_run.put(add_state)
-    return True
-
+parser.add_argument("--num-procs", type=int, default=1)
 
 if __name__ == '__main__':
-    number_of_processes = 12
+
+    args = parser.parse_args()
+
+    number_of_processes = args.num_procs
+
     states_to_run = Queue()
     processes = []
     manager = Manager()
@@ -102,9 +30,10 @@ if __name__ == '__main__':
     randomSeats = [1, 3]
     h = hashState(obs, hands)
     stateDict = manager.dict()
+    stateDetailsDict = manager.dict()
     childrenDict = manager.dict()
     parentDict = manager.dict()
-    states_to_run.put((obs, hands, seat, targetSeat, stateDict, randomSeats, childrenDict, parentDict))
+    states_to_run.put((obs, hands, seat, targetSeat, stateDict, randomSeats, childrenDict, parentDict, stateDetailsDict))
 
     # creating processes
     for w in range(number_of_processes):
@@ -115,3 +44,6 @@ if __name__ == '__main__':
     # completing process
     for p in processes:
         p.join()
+
+    print(stateDict[h])
+    print(len(stateDict.keys()))
--- direct_solving/state_value.py
+++ direct_solving/state_value.py
@@ -1,38 +1,90 @@
+from multiprocessing import current_process
+
 from hash_state import hashState
 from get_score import getScore
 from get_actions import getActions
 from new_state import newState
 
+import time
+import queue
 import numpy as np
 from numba import jit
 
 
-@jit
-def stateValue(obs, hands, seat, targetSeat, stateDict, randomSeats):
-    h = hashState(obs, hands)
-    # Don't compute the same state twice
-    if h in stateDict.keys():
-        return stateDict[h]
-
-    hand = hands[seat, :]
-    round = 6 - len(np.nonzero(hand)[0])
-    # If the game is over, compute the scores from the observation
-    if round == 6:
-        score = getScore(obs, hands, targetSeat)
-
-    # Otherwise compute the scores by recursion
-    else:
-        actions = getActions(obs, hands, seat)
-        nextStates = [newState(obs, hands, action, seat) for action in actions]
-        # Values are computed with respect to the non-random seats
-        # If it is a random turn we average since the random is uniform
-        if seat in randomSeats:
-            score = np.mean([stateValue(new_obs, new_hands, new_seat, targetSeat, stateDict, randomSeats) for
-                             (new_obs, new_hands, new_seat) in nextStates])
+def do_job(states_to_run):
+    try_again = 0
+    last_printed = -9999
+    # Continue to pull new states from the queue until they are exhausted
+    while True:
+        # This will raise queue.Empty if the queue is empty
+        try:
+            state = states_to_run.get_nowait()
+        # At the beginning there will only be one item in the queue, so I have a try again so that the 2-n processes
+        # don't quit immediately
+        except queue.Empty:
+            if try_again > 20:
+                print('Process ended')
+                break
+            time.sleep(.1*try_again)
+            try_again += 1
+        # Otherwise do the task
         else:
-            score = max([stateValue(new_obs, new_hands, new_seat, targetSeat, stateDict, randomSeats) for
-                         (new_obs, new_hands, new_seat) in nextStates])
+            # Seems to be a delay in getting things into the queue
+            try_again = 0
+            obs, hands, seat, targetSeat, stateDict, randomSeats, childrenDict, parentDict, stateDetailsDict = state
+            s = len(parentDict.keys())
+            if s - last_printed > 10000:
+                print(current_process().name)
+                print(s)
+                last_printed = s
+            h = hashState(obs, hands)
 
-    # Save the learned value and then return it
-    stateDict[h] = score
-    return score
+            # If the state has been solved before, don't do it again
+            if h in stateDict.keys():
+                continue
+
+            hand = hands[seat, :]
+            round = 6 - len(np.nonzero(hand)[0])
+
+            # If the game is over, compute the scores from the observation
+            if round == 6:
+                score = getScore(obs, hands, targetSeat)
+                # Save the learned value
+                stateDict[h] = score
+                # See if parent can be computed now, if so add back to queue
+                if h in parentDict.keys():
+                    parentHash = parentDict[h]
+                    if all(child in stateDict.keys() for child in childrenDict[parentHash]):
+                        states_to_run.put(stateDetailsDict[parentHash])
+                continue
+
+            # Find all the children and update the parent and children dicts
+            actions = getActions(obs, hands, seat)
+            nextStates = [newState(obs, hands, action, seat) for action in actions]
+            children = [hashState(new_obs, new_hands) for (new_obs, new_hands, _) in nextStates]
+            childrenDict[h] = children
+            stateDetailsDict[h] = state
+            for child in children:
+                parentDict[child] = h
+
+            # If the child states are already computed, use them
+            if all(child in stateDict.keys() for child in children):
+                # Values are computed with respect to the non-random seats
+                # If it is a random turn we average since the random is uniform
+                if seat in randomSeats:
+                    score = np.mean([stateDict[child] for child in children])
+                else:
+                    score = np.max([stateDict[child] for child in children])
+                stateDict[h] = score
+                # See if parent can be computed now, if so add back to queue
+                if h in parentDict.keys():
+                    parentHash = parentDict[h]
+                    if all(child in stateDict.keys() for child in childrenDict[parentHash]):
+                        states_to_run.put(stateDetailsDict[parentHash])
+                continue
+            # Otherwise send the child states to the compute queue
+            else:
+                for new_obs, new_hands, new_seat in nextStates:
+                    add_state = (new_obs, new_hands, new_seat, targetSeat, stateDict, randomSeats, childrenDict, parentDict, stateDetailsDict)
+                    states_to_run.put(add_state)
+    return True
